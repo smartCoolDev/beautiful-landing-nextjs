3b306d5268b3c3a28e57a9bc30a08482f8b30025
smartCoDev0629 <smart.cooldev@outlook.com>
2021-05-01 19:15:14 -0600

    finished our client testimonial carousel component

--- src/sections/testimonial.js
+++ src/sections/testimonial.js
@@ -115,8 +115,23 @@ export default function TestimonialCard() {
          {data.map((item) => (
            <Box sx={styles.reviewCard} key={item.id}>
              <Rating rating={item.review}/>
-
+             <Heading as="h3" sx={styles.title}>
+              {item.title}
+             </Heading>
+             <Text sx={styles.description}>{item.description}</Text>
+             <div className="card-footer">
+               <div className="image">
+                 <Image src={item.avatar} alt="client image" />
+               </div>
+               <div className="reviewer-info">
+                 <Heading as="h4" sx={styles.heading}>
+                   {item.name}
+                 </Heading>
+                 <Text sx={styles.designation}>{item.designation}</Text>
+               </div>
+             </div>
            </Box>
+           
          ))}
        </Carousel>
 
